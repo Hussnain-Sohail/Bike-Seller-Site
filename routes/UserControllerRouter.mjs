d65de2a95d125afaabfb33e2875fc2d316e6244b
hussnain <h63097834@gmail.com>
2026-06-08 21:00:09 +0500

    added creating account logging in account router

---
+++ routes/UserControllerRouter.mjs
@@ -0,0 +1,7 @@
+import Signup from "../controller/Signup.mts";
+import Login from '../controller/Login.mts';
+import express from 'express';
+const UserControllerRouter = express.Router();
+UserControllerRouter.post('/user/signup', Signup);
+UserControllerRouter.post('/user/login', Login);
+export default UserControllerRouter;
