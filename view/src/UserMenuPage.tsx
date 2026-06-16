1f138062b7a574bba7f8a54e581eb86b7c76bb62
hussnain <h63097834@gmail.com>
2026-06-17 03:49:54 +0500

    fixed the broken ui in user menu looks better now i know still sucks but idk

--- view/src/UserMenuPage.tsx
+++ view/src/UserMenuPage.tsx
@@ -1,38 +1,43 @@
 import { Link } from 'react-router-dom';
 import './css/UserMenuPage.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHome, faGear, faSearch } from '@fortawesome/free-solid-svg-icons';
+import { faGear, faSearch, faUser } from '@fortawesome/free-solid-svg-icons';
 
 function UserMenuePage() {
 
     return (
-        <div>
+        <div id='container'>
             <h1 id='user-menu-header'>User MENU</h1>
 
-            <div id='parent-container'>
-                <div id='font-container'>
-                    <Link to='/user/searchbike'>
-                        <FontAwesomeIcon icon={faSearch} /><br />
+            <div id='user-menu-parent'>
+                < div id='user-menu'>
+                    <Link to='/user/uploadbike'>
+                        <button className='button'>Upload Bike</button><br />
                     </Link>
-                    <Link to='/user/settings'>
-                        <FontAwesomeIcon icon={faGear} /><br />
+                    <Link to='/user/see/uploadedbikes'>
+                        <button className='button'>See Uploaded Bike</button>
                     </Link>
-                </div>
-
-                <div id='user-menu-parent'>
-                    < div id='user-menu'>
-                        <Link to='/user/uploadbike'>
-                            <button className='button'>Upload Bike</button><br />
-                        </Link>
-                        <Link to='/user/see/uploadedbikes'>
-                            <button className='button'>See Uploaded Bike</button>
-                        </Link>
-                        <Link to='/user/remove/bike'>
-                            <button className='button'>Remove Bike</button>
-                        </Link>
-                    </div>
+                    <Link to='/user/remove/bike'>
+                        <button className='button'>Remove Bike</button>
+                    </Link>
                 </div>
             </div>
+            <footer id='footer-fonts'>
+                <Link to='/user/searchbike'>
+                    <p>Search</p>
+                    <FontAwesomeIcon icon={faSearch} /><br />
+                </Link>
+
+                <Link to='/user/settings'>
+                    <p>Settings</p>
+                    <FontAwesomeIcon icon={faGear} /><br />
+                </Link>
+
+                <Link to='/user/account'>
+                    <p>Account</p>
+                    <FontAwesomeIcon icon={faUser} /><br />
+                </Link>
+            </footer>
         </div>
     );
 }
